bf484ff40687bac6c946a5ce3f85b563df604c3a
Mezdritch <mezdritch@gmail.com>
2017-11-05 16:28:08 -0500

    Dimanche

---
+++ Assets/CameraControl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+    public int speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey("d"))
+        {
+            transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey("a"))
+        {
+            transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey("s"))
+        {
+            transform.Translate(new Vector3(0, 0, -speed * Time.deltaTime));
+        }
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey("w"))
+        {
+            transform.Translate(new Vector3(0, 0, speed * Time.deltaTime));
+        }
+    }
+}
---
+++ Assets/PrimaryLoop.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PrimaryLoop : MonoBehaviour
+{
+
+    //Init, cycle,
+    private IEnumerable state;
+
+    //Stats & UI
+    private Transform cyclesUI;
+    private int cycles;
+
+
+    //////////////////////////////
+    // Start
+    void Start()
+    {
+        //Start with the Init state
+        state = Init();
+
+        //Start state machine
+        StartCoroutine(RunStateMachine());
+    }
+
+    //////////////////////////////
+    //STATE MACHINE
+    public IEnumerator RunStateMachine()
+    {
+        while (state != null)
+        {
+            foreach (var cur in state)
+            {
+                yield return cur;
+            }
+        }
+    }
+
+    //////////////////////////////
+    // INIT
+    private IEnumerable Init()
+    {
+        Debug.Log(state);
+
+        //UI
+        SetupUi();
+
+        yield return new WaitForSeconds(2);
+        state = Cycle();
+    }
+
+    //////////////////////////////
+    // CYCLE
+    private IEnumerable Cycle()
+    {
+        Debug.Log(state);
+
+        IncrementCycles();
+
+        yield return new WaitForSeconds(10);
+        state = Cycle();
+    }
+
+    //////////////////////////////
+    // Setup UI
+    private void SetupUi()
+    {
+        cyclesUI = GameObject.Find("Text_cycle_amount").transform;
+    }
+
+    //////////////////////////////
+    // Increment Cycles
+    private void IncrementCycles()
+    {
+        cycles += 1;
+        cyclesUI.GetComponent<Text>().text = (cycles).ToString();
+    }
+
+}
